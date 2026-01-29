5cd443c472cc512a4993d7d98eae91967688d1b7
Gabriel Alves <gabrielprimeiro2024@gmail.com>
2026-01-29 00:10:39 -0300

    finalizando sua maior vergonha

---
+++ fundamentos/3-secao/2-dia/exemplos/exemplo2.js
@@ -0,0 +1,12 @@
+const listaAnimais = ['Cachorro', 'Gato', 'Grilo', 'Macaco']
+
+const item = listaAnimais[2]
+
+console.log(item);
+
+
+console.log(listaAnimais[1]);
+
+listaAnimais[2] = 'Lagosta';
+
+console.log(listaAnimais);
