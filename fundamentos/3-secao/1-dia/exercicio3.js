e01dad157e9d37e46f343701853abeff02f514e2
Gabriel Alves <gabrielprimeiro2024@gmail.com>
2026-02-11 10:58:09 -0300

    add enunciado aos exercicios do dia 1

--- fundamentos/3-secao/1-dia/exercicio3.js
+++ fundamentos/3-secao/1-dia/exercicio3.js
@@ -1,4 +1,4 @@
-let pri = 165
+let pri = 15
 let seg = 15
 let ter = 8
 
@@ -11,3 +11,6 @@ if (pri > seg && pri > ter) {
 } else {
     console.log('ouxi, tem uns iguais ai');
 }
+
+//Esse código compara os valores aritiméticos das variáveis e destaca o maior entre eles, caso tenha dois de mesmo valor, será retornado a mensagem "ouxi, tem uns iguais ai"
+//Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três variáveis com os valores que serão comparados.
